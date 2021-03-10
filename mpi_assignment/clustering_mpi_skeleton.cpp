b069bc112e228ce85ccff2e46eafc9ccacb7bdfa
bella meng <dancingm1998@gmail.com>
2021-03-10 13:20:39 +0800

    rename

--- mpi_assignment/clustering_mpi_skeleton.cpp
+++ mpi_assignment/clustering_mpi_skeleton.cpp
@@ -7,75 +7,88 @@
 
 using namespace std;
 
+
 int main(int argc, char **argv) {
-  MPI_Init(&argc, &argv);
-
-  MPI_Comm comm;
-  int num_process; // number of processors
-  int my_rank;     // my global rank
-
-  comm = MPI_COMM_WORLD;
-
-  MPI_Comm_size(comm, &num_process);
-  MPI_Comm_rank(comm, &my_rank);
-
-  if (argc != 3) {
-    std::cerr << "usage: ./clustering_sequential data_path result_path"
-              << std::endl;
-
-    return -1;
-  }
-  std::string dir(argv[1]);
-  std::string result_path(argv[2]);
-
-  int num_graphs;
-  int *clustering_results = nullptr;
-  int *num_cluster_total = nullptr;
-
-  int *nbr_offs = nullptr, *nbrs = nullptr;
-  int *nbr_offs_local = nullptr, *nbrs_local = nullptr;
-
-  GraphMetaInfo *info = nullptr;
-
-  // read graph info from files
-  if (my_rank == 0) {
-    num_graphs = read_files(dir, info, nbr_offs, nbrs);
-  }
-  auto start_clock = chrono::high_resolution_clock::now();
-
-  // ADD THE CODE HERE
-
-  MPI_Barrier(comm);
-  auto end_clock = chrono::high_resolution_clock::now();
-
-  // 1) print results to screen
-  if (my_rank == 0) {
-    for (size_t i = 0; i < num_graphs; i++) {
-      printf("num cluster in graph %d : %d\n", i, num_cluster_total[i]);
+    MPI_Init(&argc, &argv);
+    
+    MPI_Comm comm;
+    int num_process; // number of processors
+    int my_rank;     // my global rank
+    
+    comm = MPI_COMM_WORLD;
+    
+    MPI_Comm_size(comm, &num_process);
+    MPI_Comm_rank(comm, &my_rank);
+    
+    if (argc != 3) {
+        std::cerr << "usage: ./clustering_sequential data_path result_path"
+        << std::endl;
+        
+        return -1;
     }
-    fprintf(stderr, "Elapsed Time: %.9lf ms\n",
-            chrono::duration_cast<chrono::nanoseconds>(end_clock - start_clock)
-                    .count() /
+    std::string dir(argv[1]);
+    std::string result_path(argv[2]);
+    
+    int num_graphs;
+    int *clustering_results = nullptr;
+    int *num_cluster_total = nullptr;
+    
+    int *nbr_offs = nullptr, *nbrs = nullptr;
+    int *nbr_offs_local = nullptr, *nbrs_local = nullptr;
+    
+    GraphMetaInfo *info = nullptr;
+    
+    // read graph info from files
+    if (my_rank == 0) {
+        num_graphs = read_files(dir, info, nbr_offs, nbrs);
+    }
+    auto start_clock = chrono::high_resolution_clock::now();
+    
+    // ADD THE CODE HERE
+    
+    // Get input, MPI_Bcast
+    
+    
+    MPI_Barrier(comm);
+    int local_num_graphs = num_graphs/num_process;
+    int local_a = my_rank*local_num_graphs;
+    int local_b = local_a + local_num_graphs;
+    for (size_t i = local_a; i < local_b; ++i) {
+        GraphMetaInfo info_local = info[i];
+        clustering_results[i] = (int *)calloc(info_local.num_vertices, sizeof(int));
+        int num_cluster_local = clustering(info_local, nbr_offs_local, nbrs_local,
+                                           clustering_results[i]);
+    }
+    auto end_clock = chrono::high_resolution_clock::now();
+    
+    // 1) print results to screen
+    if (my_rank == 0) {
+        for (size_t i = 0; i < num_graphs; i++) {
+            printf("num cluster in graph %d : %d\n", i, num_cluster_total[i]);
+        }
+        fprintf(stderr, "Elapsed Time: %.9lf ms\n",
+                chrono::duration_cast<chrono::nanoseconds>(end_clock - start_clock)
+                .count() /
                 pow(10, 6));
-  }
-
-  // 2) write results to file
-  if (my_rank == 0) {
-    int *result_graph = clustering_results;
-    for (int i = 0; i < num_graphs; i++) {
-      GraphMetaInfo info_local = info[i];
-      write_result_to_file(info_local, i, num_cluster_total[i], result_graph,
-                           result_path);
-
-      result_graph += info_local.num_vertices;
     }
-  }
-
-  MPI_Finalize();
-
-  if (my_rank == 0) {
-    free(num_cluster_total);
-  }
-
-  return 0;
+    
+    // 2) write results to file
+    if (my_rank == 0) {
+        int *result_graph = clustering_results;
+        for (int i = 0; i < num_graphs; i++) {
+            GraphMetaInfo info_local = info[i];
+            write_result_to_file(info_local, i, num_cluster_total[i], result_graph,
+                                 result_path);
+            
+            result_graph += info_local.num_vertices;
+        }
+    }
+    
+    MPI_Finalize();
+    
+    if (my_rank == 0) {
+        free(num_cluster_total);
+    }
+    
+    return 0;
 }
